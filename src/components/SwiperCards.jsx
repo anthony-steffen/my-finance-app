5c099d8d42af575269dd0e8056edf9a1ec769bc8
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-01-17 01:47:59 -0300

    :sparkles: feat: Implementando função para dividir o número de cards criados por interação.
    Implementando função para fracionar o array transactions em grupo de três para que cada interação crie 3 cards para cada item do carousel.

--- src/components/SwiperCards.jsx
+++ src/components/SwiperCards.jsx
@@ -10,7 +10,19 @@ import HomeContext from '../contexts/HomeContext';
 
 function SwiperCards() {
   const { transaction, categoryIcons } = useContext(HomeContext);
-  console.log(transaction);
+
+  // Função para dividir o array de transações em array de 3 elementos
+  const splitArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  };
+
+  // Divide as transações em grupos de três
+  const set = 3;
+  const transactionGroup = splitArray(transaction, set);
 
   return (
     <div
@@ -24,25 +36,30 @@ function SwiperCards() {
         Gastos por Categoria
       </h5>
       <div className="carousel-inner">
-        <div className="carousel-item active">
-          {transaction.map((element) => (
-            <div className="category-card" key={ element.id }>
-              <div className="category-card-body">
-                <div className="category-card-title  mb-0">{element.category}</div>
-                <div className="category-card-icon">
-                  {categoryIcons[element.category]}
-                </div>
-                <div className="category-card-value">
-                  <span className="text-dark">
-                    R$
-                    {element.value}
-                  </span>
+        {transactionGroup.map((group, index) => (
+          <div
+            key={ index }
+            className={ `carousel-item ${index === 0 ? 'active' : ''}` }
+          >
+            {group.map((element) => (
+              <div className="category-card" key={ element.id }>
+                <div className="category-card-body">
+                  <div className="category-card-title mb-0">{element.category}</div>
+                  <div className="category-card-icon">
+                    {categoryIcons[element.category]}
+                  </div>
+                  <div className="category-card-value">
+                    <span className="text-dark">
+                      R$
+                      {' '}
+                      {element.value}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-
+            ))}
+          </div>
+        ))}
       </div>
       <button
         className="carousel-control-prev"
