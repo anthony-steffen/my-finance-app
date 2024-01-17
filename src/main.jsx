592e70a888c0bc8840449c79c67e79fe12f84177
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-01-17 18:15:01 -0300

    fix
    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import Home from './pages/Home';
@@ -9,12 +9,12 @@ import AuthProvider from './contexts/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <BrowserRouter basename="/my-finance-app">
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={ <App /> } />
-        <Route path="register" element={ <Register /> } />
+        <Route path="/register" element={ <Register /> } />
         <Route path="/home" element={ <Home /> } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>,
 );
