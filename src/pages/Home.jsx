c5195451266d0015b1f7638c91b07a2580737134
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-02-13 21:01:43 -0300

    :construction: funcionalidade android backPressed
     funcionalidade para android backPressed para tratar comportamento do botão < "voltar" em dispositivos móveis, em construção.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // Home.jsx
-import React from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import HomeProvider from '../contexts/HomeProvider';
 import ExpenseProvider from '../contexts/ExpenseProvider';
 import IncomeProvider from '../contexts/IncomeProvider';
@@ -7,6 +8,39 @@ import Content from '../components/Content';
 import '../styles/components/ToggleTheme.css';
 
 function Home() {
+  const navigate = useNavigate();
+  const backPressed = useRef(false);
+
+  const handleBackButton = useCallback(
+    () => {
+      if (backPressed.current) {
+      // Se o botão de voltar foi pressionado novamente, redirecione para a raiz
+        navigate('/');
+      } else {
+      // Se o botão de voltar foi pressionado pela primeira vez, defina backPressed como true
+        backPressed.current = true;
+        // Exiba uma mensagem para o usuário
+        alert('Pressione novamente para sair');
+        // Defina backPressed como false após 2 segundos
+        setTimeout(() => {
+          backPressed.current = false;
+        }, 2000);
+      }
+    },
+    [navigate],
+  );
+
+  useEffect(
+    () => {
+    // Adiciona um ouvinte de evento para o botão de voltar
+      window.addEventListener('popstate', handleBackButton);
+      // Remove o ouvinte de evento quando o componente é desmontado
+      return () => {
+        window.removeEventListener('popstate', handleBackButton);
+      };
+    },
+    [handleBackButton],
+  );
   return (
     <div className="home-container">
       <HomeProvider>
